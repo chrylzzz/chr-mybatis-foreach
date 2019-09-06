2818450f20f4e781d859abaf529b5b9b7432d890
chryl <cyl5yy412@gmail.com>
2019-09-06 21:55:22 +0800

    init

--- src/main/java/com/chryl/mapper/ForeachMapper.java
+++ src/main/java/com/chryl/mapper/ForeachMapper.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chryl on 2019/9/6.
@@ -12,7 +13,7 @@ import java.util.List;
 public interface ForeachMapper {
     int inserts(@Param("name") String name, @Param("params") List<String> ids);
 
-    int inserts2(@Param("name") String name, @Param("params") List<String> ids);
-
     int deleteIds(@Param("params") List<String> ids);
+
+    int updateBatch(@Param("params") Map<String,String> map);
 }
--- src/test/java/com/chryl/ChrMybatisForeachApplicationTests.java
+++ src/test/java/com/chryl/ChrMybatisForeachApplicationTests.java
@@ -8,47 +8,55 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ChrMybatisForeachApplicationTests {
 
-	@Resource
-	private ForeachMapper foreachMapper;
-	@Test
-	public void contextLoads() {
-		List<String> ids=new ArrayList<>();
-		ids.add("1");
-		ids.add("123");
-		ids.add("124");
-		ids.add("125");
-		ids.add("1256");
-		ids.add("234");
-		ids.add("5463");
-		int i = foreachMapper.deleteIds(ids);
-		System.out.println(i);
-	}
-	@Test
-	public void contextLoads2() {
-		List<String> ids=new ArrayList<>();
-		ids.add("123");
-		ids.add("122");
-		ids.add("1133");
-		ids.add("125");
-		ids.add("113");
-		int nancy = foreachMapper.inserts("nancy", ids);
-		System.out.println(nancy);
-	}
-	@Test
-	public void contextLoads3() {
-		List<String> ids=new ArrayList<>();
-		ids.add("223");
-		ids.add("224");
-		ids.add("225");
-		ids.add("226");
-		ids.add("217");
-		int nancy = foreachMapper.inserts("nancy", ids);
-		System.out.println(nancy);
-	}
+    @Resource
+    private ForeachMapper foreachMapper;
+
+    @Test
+    public void contextLoads() {
+        List<String> ids = new ArrayList<>();
+        ids.add("1");
+        ids.add("123");
+        ids.add("124");
+        ids.add("125");
+        ids.add("1256");
+        ids.add("234");
+        ids.add("5463");
+        int i = foreachMapper.deleteIds(ids);
+        System.out.println(i);
+    }
+
+    @Test
+    public void contextLoads2() {
+        List<String> ids = new ArrayList<>();
+        ids.add("123");
+        ids.add("122");
+        ids.add("1133");
+        ids.add("125");
+        ids.add("113");
+        int nancy = foreachMapper.inserts("nancy", ids);
+        System.out.println(nancy);
+    }
+
+    @Test
+    public void contextLoads3() {
+        Map<String, String> map = new HashMap<>();
+        map.put("123", "nanc");
+        map.put("122", "nanc");
+        map.put("1133", "nanc");
+        map.put("113", "nanc");
+        map.put("125", "nanc");
+        map.put("157", "nanc");
+        map.put("23", "nanc");
+        int i = foreachMapper.updateBatch(map);
+        System.out.println(i);
+
+    }
 }
